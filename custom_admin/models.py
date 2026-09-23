2a75142e50e2f2c12b673c5983eb92d19479054e
codewithrajesh <rajeshlagdhir2212@gmail.com>
2026-09-23 09:57:22 +0530

    feat: complete enterprise logistics platform with real-time tracking, payments, and admin panel

--- custom_admin/models.py
+++ custom_admin/models.py
@@ -5,28 +5,6 @@ def generate_tracking_id():
     return "PP" + uuid.uuid4().hex[:8].upper()
 
 # Create your models here.
-class Book_parcel(models.Model):
-                
-    sender_name = models.CharField(max_length=50)
-    receiver_name = models.CharField(max_length=50)
-    mobile_number = models.CharField(max_length=10)
-    parcel_weight = models.CharField(max_length=4)
-    pickup_address = models.CharField(max_length=50)
-    delivery_address = models.CharField(max_length=50)
-    parcel_type = models.CharField(max_length=10)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    tracking_id = models.CharField(
-            max_length=20,
-            unique=True,
-            default=generate_tracking_id
-        )
-    status = models.CharField(max_length=50,default="Parcel Booked")
-    qr_code = models.ImageField(upload_to='qr_codes/', blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.tracking_id:
-            self.tracking_id = "PP" + uuid.uuid4().hex[:8].upper()
-        super().save(*args, **kwargs)
 
 class Contact(models.Model):
 
